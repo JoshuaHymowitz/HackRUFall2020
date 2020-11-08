3abe717cd4f2d1610de19042653139005c21720e
JoshuaHymowitz <46762344+JoshuaHymowitz@users.noreply.github.com>
2020-11-07 21:22:31 -0500

    TweetScraper

--- src/TweetScraping.py
+++ src/TweetScraping.py
@@ -12,19 +12,37 @@ import pandas as pd
 
 import snscrape.modules.twitter as sntwitter
 import csv
+import sys
+import numpy
+import os
+
+numpy.set_printoptions(threshold=sys.maxsize)
+
+stream = os.popen('snscrape --max-results 10 twitter-hashtag election')
+output = stream.read()
+print(output)
+
+#^^^ prints the last 10 tweets with the hashtag 'election'
+
+#below is wip and loads tweets into csv
+
 maxTweets = 3000
 
-csvFile = open('place_result.csv', 'a', newline='', encoding='utf8')
+csvFile = open('place_result.csv', 'w+', newline='', encoding='utf8')
 
 csvWriter = csv.writer(csvFile)
 csvWriter.writerow(['id','date','tweet',])
 
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:@burakoglakci + since:2015-12-02 until:2020-11-05-filter:links -filter:replies').get_items()):
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:@realDonaldTrump + from:@JoeBiden').get_items()):
     if i > maxTweets :
         break
     csvWriter.writerow([tweet.id, tweet.date, tweet.content])
 csvFile.close()
 
+col_list = ["id", "date", "tweet"]
+df = pd.read_csv("place_result.csv", usecols=col_list)
+
+print('[%s]' % '\n \n'.join(map(str, df["tweet"])))
 
 username = "BarackObama"
 text_query = "Hello"
