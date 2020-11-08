469f0cd60b101238f105418dafef26255455efd9
JoshuaHymowitz <46762344+JoshuaHymowitz@users.noreply.github.com>
2020-11-07 23:49:12 -0500

    Updating TweetScraping

--- src/TweetScraping.py
+++ src/TweetScraping.py
@@ -26,45 +26,56 @@ print(output)
 
 #below is wip and loads tweets into csv
 
-maxTweets = 3000
+maxTweets = 100
 
 csvFile = open('place_result.csv', 'w+', newline='', encoding='utf8')
 
 csvWriter = csv.writer(csvFile)
 csvWriter.writerow(['id','date','tweet',])
 
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:@realDonaldTrump + from:@JoeBiden').get_items()):
-    if i > maxTweets :
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:@realDonaldTrump').get_items()):
+    if i > maxTweets / 2 :
         break
     csvWriter.writerow([tweet.id, tweet.date, tweet.content])
+  
+    i = maxTweets / 2;
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('twitter-hashtag:election').get_items()):
+    if i > maxTweets:
+        break
+    csvWriter.writerow([tweet.id, tweet.date, tweet.content])
+    
 csvFile.close()
+
+
 
 col_list = ["id", "date", "tweet"]
 df = pd.read_csv("place_result.csv", usecols=col_list)
 
 print('[%s]' % '\n \n'.join(map(str, df["tweet"])))
 
-username = "BarackObama"
-text_query = "Hello"
-since_date = "2011-01-01"
-until_date = "2016-12-20"
-count = 150
+
+
+# username = "BarackObama"
+# text_query = "Hello"
+# since_date = "2011-01-01"
+# until_date = "2016-12-20"
+# count = 150
  
-# Creation of tweetCriteria query object with methods to specify further
-tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
-    .setQuerySearch(text_query).setSince(since_date)\
-        .setUntil(until_date).setMaxTweets(count)
+# # Creation of tweetCriteria query object with methods to specify further
+# tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
+#     .setQuerySearch(text_query).setSince(since_date)\
+#         .setUntil(until_date).setMaxTweets(count)
  
 
-# Creation of tweets iterable containing all queried tweet data
-#tweets = got.manager.TweetManager.getTweets(tweetCriteria) #this line is 404ing
+# # Creation of tweets iterable containing all queried tweet data
+# #tweets = got.manager.TweetManager.getTweets(tweetCriteria) #this line is 404ing
  
-# List comprehension pulling chosen tweet information from tweets
-# Add or remove tweet information you want in the below list comprehension
-#tweets_list = [[tweet.id, tweet.author_id, tweet.username, tweet.text, tweet.retweets, tweet.favorites,tweet.replies,tweet.date] for tweet in tweets]
+# # List comprehension pulling chosen tweet information from tweets
+# # Add or remove tweet information you want in the below list comprehension
+# #tweets_list = [[tweet.id, tweet.author_id, tweet.username, tweet.text, tweet.retweets, tweet.favorites,tweet.replies,tweet.date] for tweet in tweets]
  
-# Creation of dataframe from tweets list
-# Add or remove columns as you remove tweet information
-#tweets_df3 = pd.DataFrame(tweets_list, columns = ['Tweet Id', 'Tweet User Id', 'Tweet User', 'Text','Retweets', 'Favorites', 
- #                                                'Replies', 'Datetime'])
+# # Creation of dataframe from tweets list
+# # Add or remove columns as you remove tweet information
+# #tweets_df3 = pd.DataFrame(tweets_list, columns = ['Tweet Id', 'Tweet User Id', 'Tweet User', 'Text','Retweets', 'Favorites', 
+#  #                                                'Replies', 'Datetime'])
 
